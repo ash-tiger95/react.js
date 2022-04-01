6acabf27f491fd026a6e748515fa7cda662e8535
SungHo Ahn <jugiaro95@gmail.com>
2022-04-01 23:49:06 +0900

    :o: func: more page

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import { Icon, Card, Col, Row } from "antd";
+import axios from "axios";
+import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
+  const [products, setProducts] = useState([]);
+  const [skip, setSkip] = useState(0); // 몽고DB skip함수를 위함
+  const [limit, setLimit] = useState(8); // 몽고DB limit함수를 위함
+  const [postSize, setPostSize] = useState(0);
+
+  useEffect(() => {
+    let body = {
+      skip: skip,
+      limit: limit,
+    };
+
+    getProducts(body);
+  }, []);
+
+  const getProducts = (body) => {
+    axios.post("/api/products/products", body).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        if (body.loadMore) {
+          setProducts([...products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
+      } else {
+        alert("상품들을 가져오는데 실패했습니다.");
+      }
+    });
+  };
+
+  const loadMoreHandler = () => {
+    let skipChange = skip + limit;
+    let body = {
+      skip: skipChange,
+      limit: limit,
+      loadMore: true,
+    };
+
+    getProducts(body);
+    setSkip(skipChange);
+  };
+
+  const renderCards = products.map((product, index) => {
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere <Icon type="rocket" />
+        </h2>
+      </div>
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      {postSize >= limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={loadMoreHandler}>Load More</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
--- boilerplate-mern-stack-master/server/routes/products.js
+++ boilerplate-mern-stack-master/server/routes/products.js
@@ -43,4 +43,21 @@ router.post("/", (req, res) => {
   });
 });
 
+router.post("/products", (req, res) => {
+  // product collection에 들어 있는 모든 상품 가져오기
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0; // 0번째부터
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+
+  Product.find()
+    .populate("writer") // 이 사람에 대한 정보 가져오기 (DB에서 writer의 정보)
+    .skip(skip) // (몽고DB 함수) skip부터 (어디서부터 데이터를 가져오는지에 대한 위치)
+    .limit(limit) // (몽고DB 함수) limit 개수만큼 가져오기 (처음 데이터를 가져올때와 더보기 버튼을 눌러서 가져올 때 얼마나 많은 데이터를 한번에 가져오는지)
+    .exec((err, productInfo) => {
+      if (err) return res.status(400).json({ success: false, err });
+      return res
+        .status(200)
+        .json({ success: true, productInfo, postSize: productInfo.length });
+    });
+});
+
 module.exports = router;
