80d34dfaeccee253c314b3dbb30bc9be007e3fd5
SungHo Ahn <jugiaro95@gmail.com>
2022-04-01 22:30:24 +0900

    :o: UploadProduct (FE, BE)

---
+++ boilerplate-mern-stack-master/client/src/components/utils/FileUpload.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import { Icon } from "antd";
+import axios from "axios";
+
+const FileUpload = (props) => {
+  const [images, setImages] = useState([]);
+
+  /* 백으로 이미지 전달 */
+  const dropHandler = (files) => {
+    let formData = new FormData(); // 파일을 보낼 때 필수
+    formData.append("file", files[0]);
+
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+
+    axios.post("/api/products/image", formData, config).then((response) => {
+      if (response.data.success) {
+        setImages([...images, response.data.filePath]);
+        props.refreshFunction([...images, response.data.filePath]);
+      } else {
+        alert("파일을 저장하는데 실패했습니다.");
+      }
+    });
+  };
+  const deleteHandler = (image) => {
+    const currentIndex = images.indexOf(image);
+
+    let newImages = [...images];
+    newImages.splice(currentIndex, 1); // currentIndex부터 +1 미만까지 삭제
+
+    setImages(newImages);
+    props.refreshFunction(newImages);
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Dropzone onDrop={dropHandler}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div
+              style={{
+                width: 300,
+                height: 240,
+                border: "1px solid lightgray",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              <Icon type="plus" style={{ fontSize: "3rem" }} />
+            </div>
+          </section>
+        )}
+      </Dropzone>
+
+      <div
+        style={{
+          display: "flex",
+          width: "350px",
+          height: "240px",
+          overflowX: "scroll",
+        }}
+      >
+        {images.map((image, index) => (
+          <div key={index} onClick={() => deleteHandler(image)}>
+            <img
+              style={{ minWidth: "300px", width: "300px", height: "240px" }}
+              src={`http://localhost:5000/${image}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FileUpload;
